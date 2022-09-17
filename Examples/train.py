c253d8ce5132cbd91e203d2f59aba1ce5f6a5027
shepai <dextershepherd@hotmail.co.uk>
2022-09-17 12:35:05 +0100

    no bias

--- Examples/train.py
+++ Examples/train.py
@@ -10,7 +10,7 @@ def activation(nump):
 #create neural network
 net=Network(2)
 net.add_layer(2,vals=np.array([0,0,0,0]))
-net.add_bias(vals=np.array([0,0]))
+#net.add_bias(vals=np.array([1,1]))
 net.show()
 
 #set up logic gate data and expected outcome
@@ -20,4 +20,4 @@ y=np.array([[0],[0],[0],[1]])
 #run training loop    
 net.train(X_data,y,10,activation,0.05)
 
-print(net.network[0].matrix,net.network[0].bias)
+print(net.network[0].matrix)
