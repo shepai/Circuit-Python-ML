4da66a8cadce9641f4ab8ec7717a525bbb77a46d
shepai <dextershepherd@hotmail.co.uk>
2022-09-17 10:24:38 +0100

    examples working

--- Examples/AND gate.py
+++ Examples/AND gate.py
@@ -2,9 +2,7 @@ from CPML import *
 import ulab.numpy as np
 
 def activation(nump):
-    print(nump)
     s=np.sum(nump)
-    print(s)
     if s>=0:
         return 1
     return 0
@@ -15,21 +13,21 @@ net.add_bias(vals=np.array([-1,-1]))
 net.show()
 
 #off
-x=np.array([0,0])
-num=net.forward(x).transpose()[0]
-print("******",activation(num))
-
-#off
-x=np.array([0,1])
+x=np.array([[0],[0]])
 num=net.forward(x).transpose()[0]
 print(activation(num))
 
 #off
-x=np.array([1,0])
+x=np.array([[0],[1]])
+num=net.forward(x).transpose()[0]
+print(activation(num))
+
+#off
+x=np.array([[1],[0]])
 num=net.forward(x).transpose()[0]
 print(activation(num))
 
 #on
-x=np.array([1,1])
+x=np.array([[1],[1]])
 num=net.forward(x).transpose()[0]
 print(activation(num))
--- Examples/OR gate.py
+++ Examples/OR gate.py
@@ -1,12 +1,33 @@
 from CPML import *
 import ulab.numpy as np
 
+def activation(nump):
+    s=np.sum(nump)
+    if s>0:
+        return 1
+    return 0
+
 net=Network(2)
-net.add_layer(2,vals=np.array([0,0,0,0]))
-net.add_bias(vals=np.array([-1,1]))
+net.add_layer(2,vals=np.array([1,0,1,0]))
+net.add_bias(vals=np.array([0,0]))
 
 
-x=np.array([0,0])
+#off
+x=np.array([[0],[0]])
+num=net.forward(x).transpose()[0]
+print(activation(num))
 
-num=net.forward(x)
-print(num)
+#on
+x=np.array([[0],[1]])
+num=net.forward(x).transpose()[0]
+print(activation(num))
+
+#on
+x=np.array([[1],[0]])
+num=net.forward(x).transpose()[0]
+print(activation(num))
+
+#on
+x=np.array([[1],[1]])
+num=net.forward(x).transpose()[0]
+print(activation(num))
