542bd3b28f2b984088de5e92cbed93cb4a0272f0
shepai <dextershepherd@hotmail.co.uk>
2022-09-17 14:57:02 +0100

    chaotic atrractor

---
+++ CPML/chaos/__init__.py
@@ -0,0 +1,39 @@
+import ulab.numpy as np
+
+def lorenz(x, y, z, s=10, r=28, b=2.667):
+    """
+    Given:
+       x, y, z: a point of interest in three dimensional space
+       s, r, b: parameters defining the lorenz attractor
+    Returns:
+       x_dot, y_dot, z_dot: values of the lorenz attractor's partial
+           derivatives at the point x, y, z
+    """
+
+    x_dot = s*(y - x)
+    y_dot = r*x - y - x*z
+    z_dot = x*y - b*z
+    return x_dot, y_dot, z_dot
+
+
+dt = 0.01
+num_steps = 1000
+
+def get_plot(s,p,b):
+    # Need one more for the initial values
+    xs = np.zeros(num_steps + 1)
+    ys = np.zeros(num_steps + 1)
+    zs = np.zeros(num_steps + 1)
+
+    # Set initial values
+    xs[0], ys[0], zs[0] = (0., 1., 1.05)
+
+    # Step through "time", calculating the partial derivatives at the current point
+    # and using them to estimate the next point
+    for i in range(num_steps):
+        x_dot, y_dot, z_dot = lorenz(xs[i], ys[i], zs[i],s=s,r=p,b=b)
+        xs[i + 1] = xs[i] + (x_dot * dt)
+        ys[i + 1] = ys[i] + (y_dot * dt)
+        zs[i + 1] = zs[i] + (z_dot * dt)
+    return xs,ys,zs
+
