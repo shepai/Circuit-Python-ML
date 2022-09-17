9a769d017492b3994ad437a96d6b231e61edd5b7
shepai <dextershepherd@hotmail.co.uk>
2022-09-17 09:24:53 +0100

    example code

---
+++ Examples/OR gate.py
@@ -0,0 +1,13 @@
+from CPML import *
+import ulab.numpy as np
+
+net=Network(2)
+net.add_layer(2,vals=np.array([1,1,1,1]))
+net.add_bias(vals=np.array([0,0,0,0]))
+net.add_layer(2,vals=np.array([1,1,1,1]))
+net.add_bias(vals=np.array([0,0,0,0]))
+
+x=np.array([1,0])
+
+num=net.forward(x)
+print(num)
--- Library/CPML.py
+++ Library/CPML.py
@@ -25,18 +25,43 @@ def normal(Mean=0,StdDev=0.5,size=[5]):
     return ar.reshape(shape)
 
 class Layer:
-    def __init__(self,nodes_in,nodes_out):
-        self.matrix=normal(size=(nodes_in,nodes_out)) #generate random weights
+    def __init__(self,nodes_in,nodes_out,vals=None):
+        self.vals=vals
+        if vals!=None:
+            self.matrix=vals.reshape((nodes_in,nodes_out)) #generate random weights
+        else:
+            self.matrix=normal(size=(nodes_in,nodes_out)) #generate random weights
     def __mul__(self,other):
-        return np.dot(self.matrix,other) #multiply the matrices together
+        return np.dot(other,self.matrix) #multiply the matrices together
     def getShape(self): #return the shape of the matrix
         return self.matrix.shape
-class network:
-    def __init__(self):
+class Network:
+    def __init__(self,num_out):
         self.network=[]
-    def add_layer(self,nodes):
-        layer=None
+        self.num_out=num_out
+    def add_layer(self,nodes,vals=None):
+        layer=Layer(nodes,self.num_out,vals=vals) #default x by y
         if len(self.network)>0: #there are previous nodes
-            num=self.network[-1].shape[1]
-            layer=Layer(num,nodes) #generate layer with correct matrices
-        self.network.append(layer) #add the layer to the network
+            layer=self.network[-1][0]
+            bias=self.network[-1][1]
+            num=layer.getShape()
+            val=layer.vals
+            self.network[-1]=[Layer(num[0],nodes,vals=val),bias] #correct output of matrices before
+            layer=Layer(nodes,self.num_out,vals=vals) #generate layer with correct matrices
+        self.network.append([layer,None]) #add the layer to the network
+    def add_bias(self,vals=None):
+        assert len(self.network)>0, "Network is empty. Add layers"
+        size=self.network[-1][0].getShape()[0] #get the end sizing to add on
+        if vals==None:
+            vals=normal(size=(size,1))
+        self.network[-1]=[self.network[-1][0],vals]
+    def forward(self,inp):
+        assert len(self.network)>0, "Network is empty. Add layers"
+        x=inp * self.network[0].matrix
+        sub=self.network[1:-1]
+        for layer in sub:
+            x = x * layer
+        return x
+        
+
+
