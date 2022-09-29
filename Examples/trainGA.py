d94e548f68408f5a6a6cdc7bae93ff7c8840c90c
shepai <dextershepherd@hotmail.co.uk>
2022-09-29 11:07:32 +0100

    accuracy preds

---
+++ Examples/trainGA.py
@@ -0,0 +1,17 @@
+from CPML import *
+import ulab.numpy as np
+
+#create neural network
+net=Network(2)
+net.add_layer(2)
+net.add_layer(5)
+net.show()
+
+#set up logic gate data and expected outcome
+X_data=np.array([[0,0],[0,1],[1,0],[1,1]])
+y=np.array([[0,0],[1,1],[1,1],[0,0]])
+
+
+#run training loop    
+net.trainGA(X_data,y,100,0.2)
+
